eb6da1a21e14a14417d72cbee3fe7e3f55c0c5b6
Aditya Singh <as1671109@gmail.com>
2024-02-12 11:12:30 +0530

    Done with Exception Handling

---
+++ Dictionaries.py
@@ -0,0 +1,8 @@
+dic = {
+    "Aditya":"Human",
+    "Spoon":"Object"
+}
+print(dic)
+print(dic["Aditya"])
+print(dic.get("Aditya"))
+print(dic.update("Spoon","Utensils"))
---
+++ Sets.py
@@ -0,0 +1,5 @@
+number = [1,2,3,3,3,4,5,6,6]
+numberSet = set(number)
+print(numberSet)
+anotherSet = {1,2,3,3,3,4,5,6}
+print(type(anotherSet))
